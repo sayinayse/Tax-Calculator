b933eba6c86580889ca323458700746c6b80808c
sayinayse <45228078+sayinayse@users.noreply.github.com>
2020-09-19 16:15:33 +0300

    Add files via upload
    Receipts are created but test.txt did not used.

--- Main.java
+++ Main.java
@@ -7,24 +7,24 @@ enum Type{
 
 class Item{
 	private String name;
-	private float price;
+	private double price;
 	private Type type;
 	private boolean isExempted;
 	private boolean isImported;
 	
-	public Item(String name, float price, Type itemType, boolean isExempted, boolean isImported){
+	public Item(String name, double price, Type itemType){
 		this.name = name;
 		this.type = itemType;
-		this.isExempted = isExempted;
-		this.isImported = isImported;
 		this.setPrice(price);
+		this.setIsExempted();
+		this.setIsImported();
 	}
 	
-	public float getPrice(){
+	public double getPrice(){
 		return price;
 	}
 
-	public void setPrice(float price){
+	public void setPrice(double price){
 		this.price = price;
 	}
 	
@@ -40,8 +40,14 @@ class Item{
 		return isExempted;
 	}
 	
-	public void setIsExempted(boolean isExempted){
-		this.isExempted = isExempted;
+	public void setIsExempted(){
+		if(this.getName().contains("book") || this.getName().contains("phill") || this.getName().contains("chocolate")){
+			this.isExempted = true;
+		}
+		else{
+			this.isExempted = false;
+		}
+		
 	}
 	
 	
@@ -49,20 +55,70 @@ class Item{
 		return isImported;
 	}
 	
-	public void setIsImported(boolean isImported){
-		this.isImported = isImported;
+	public void setIsImported(){
+		if(this.name.contains("imported")){
+			this.isImported = true;
+		}
+		else{
+			this.isImported = false;
+		}
+	
 	}
 	
 }
 
+class Receipt{
+	private double total;
+	
+	public double getTotal(){
+		return total;
+	}
+	
+	public void setTotal(double totalPrice){
+		this.total = totalPrice;
+	}
+	
+	public void calculateTax(Item item){
+		if(item.getIsExempted() && item.getIsImported() != true){
+			this.setTotal(item.getPrice());
+		}
+		
+		if(item.getIsExempted() && item.getIsImported()){
+			this.setTotal(item.getPrice() + (item.getPrice() * 0.05));	
+		}
+		
+		if(item.getIsExempted() != true && item.getIsImported()){
+			this.setTotal(item.getPrice() + (item.getPrice() * 0.15));
+		}
+		um. Complex
+		if(item.getIsExempted() != true && item.getIsImported() != true){
+			this.setTotal(item.getPrice() + (item.getPrice() * 0.10));
+		}
+		
+	}
+}
 
 public class Main{
 	
 	public static void main(String[] args){
 		
-		Item item1 = new Item("book", 12.49, );
+		Item item1 = new Item("book", 12.49, Type.BOOK);
+		Item item2 = new Item("music CD", 14.99 ,Type.OTHER);
+		/*System.out.println(item1.getName());
+		System.out.println(" ");
+		System.out.println(item1.getIsExempted());
+		System.out.println(" ");
+		System.out.println(item1.getIsImported());*/
 	
-		//Receipt receipt1 = new Receipt();
+	
+		Receipt receipt1 = new Receipt();
+		receipt1.calculateTax(item1);
+		System.out.println(receipt1.getTotal());
+		
+		Receipt receipt2 = new Receipt();
+		receipt2.calculateTax(item2);
+		System.out.println(receipt2.getTotal());
+		
 	
 	
 	}
